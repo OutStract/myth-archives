058438015a8620f94d7c21a9c0813a1045d4b6da
Siddharth <siddharthsharma2006@gmail.com>
2026-02-12 22:25:48 +0530

    Other files

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import {mainData, sideData} from '@/lib/story.js'
 import StoryCard from '@/components/storyCard'
-import Underline from '@/components/Underline'
 
 
 function home() {
@@ -11,42 +10,40 @@ function home() {
         <>
             <h1 className='welcome'>Welcome to the archives</h1>
             <div className='home-body'>
-            <div className='story-line'>
-            <h2 className='line'>Main Line</h2>
-            <div className="underline"></div>
+                    <div className='story-line'>
+                        <h2 className='line'>Main Line</h2>
+                        <div className="underline"></div>
+                    </div>
+
+                <div className='card-group'>
+
+                    {mainData.map (story => (
+                        <StoryCard 
+                        key={story.id}
+                        story = {story.story}
+                        synopsis = {story.synopsis}
+                        storyLink= {story.slug}
+                        />
+                    ))}
+
+                </div>
+
+                <div className='story-line'>
+                <h2 className='line'>Side Line</h2>
+                <div className="underline"></div>
+                </div>
+
+                <div className='card-group'>
+                    {sideData.map (story => (
+                        <StoryCard 
+                        key={story.id}
+                        story = {story.story}
+                        synopsis = {story.synopsis}
+                        storyLink= {story.slug}
+                        />
+                    ))}
+                </div>
             </div>
-
-            <div className='card-group'>
-
-                {mainData.map (story => (
-                    <StoryCard 
-                    key={story.id}
-                    story = {story.story}
-                    synopsis = {story.synopsis}
-                    storyLink= {story.slug}
-                    />
-                ))}
-
-            </div>
-
-        <div className='story-line'>
-        <h2 className='line'>Side Line</h2>
-        <div className="underline"></div>
-        </div>
-
-        <div className='card-group'>
-            {sideData.map (story => (
-                <StoryCard 
-                key={story.id}
-                story = {story.story}
-                synopsis = {story.synopsis}
-                storyLink= {story.slug}
-                />
-            ))}
-        </div>
-
-
-        </div>
         
         </>
     )
